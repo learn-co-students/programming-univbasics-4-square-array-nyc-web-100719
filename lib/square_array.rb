ba8d65d2d889f1977bb081954d677c7f871135e1
Hector Polanco <hdp2112@columbia.edu>
2019-09-18 23:05:41 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,14 @@
+array = [1, 2, 3];
+
 def square_array(array)
-  # your code here
+  square = [];
+  counter = 0;
+  
+  while counter < array.size do
+    square.push(array[counter] ** 2);
+    counter += 1;
+  end
+  return square;
 end
+
+square_array(array)
