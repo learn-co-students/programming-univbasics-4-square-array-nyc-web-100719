771bc6e212eb91533f17ae50ffeef6c79f9dd50d
Hector Polanco <hdp2112@columbia.edu>
2019-09-18 23:08:15 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,9 @@
 def square_array(array)
   square = [];
   counter = 0;
-  
   while counter < array.size do
-    square.push(array[counter] ** 2);
+    square << array[counter] ** 2;
     counter += 1;
   end
-  return square;
+  square
 end
-
-square_array(array)
